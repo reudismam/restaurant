85143aec032fccfcf9447a177f77f5e6af7c2dfd
reudismam <reudismam@gmail.com>
2020-05-18 08:36:50 -0300

    life cycle methods.

--- src/components/DishDetails/index.js
+++ src/components/DishDetails/index.js
@@ -1,23 +1,39 @@
-import React, {useState} from 'react';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
+import React, { Component, useState } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-export default function DishDetails(props) {
-    const dish = props.dish;
-    if (dish != null) {
-        return (
-            <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-        );
+class DishDetails extends Component {
+    constructor(props) {
+        super(props);
     }
-    else {
-        return (
-            <div>
-            </div>
-        );
+
+    componentDidMount() {
+        console.log("DishDetail component mounted.");
+    }
+
+    componentDidUpdate() {
+        console.log("DishDetail component updated.");
+    }
+
+    render() {
+        const dish = this.props.dish;
+        if (dish != null) {
+            return (
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else {
+            return (
+                <div>
+                </div>
+            );
+        }
     }
 }
+
+export default DishDetails;
